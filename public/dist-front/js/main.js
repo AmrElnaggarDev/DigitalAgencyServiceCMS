3f14bcba6512360a21dbe34ae14050836a37792c
Amr Elnaggar <UG_31202189@ics.tanta.edu.eg>
2026-09-24 00:42:19 +0300

    feat(slider): implement slider edit, update, delete logic and dynamic datatable views

--- public/dist-front/js/main.js
+++ public/dist-front/js/main.js
@@ -14,11 +14,11 @@
 11. Product Slider 2 Js
 12. Product Slider 3 Js
 13. Product Slider 4 Js
-14. Sale Slider Js 
-15. Sale Slider 2 Js 
+14. Sale Slider Js
+15. Sale Slider 2 Js
 16. Client Slider Js
 17. Blog Slider Js
-18. Product Offer SLider Js 
+18. Product Offer SLider Js
 19. Masonary Js
 20. WoW Js
 21. Cart Plus Minus Js
@@ -160,25 +160,25 @@
 		animated_swiper(sliderActive1, sliderInit1);
 	}
 
-	// team 
+	// team
 	if (jQuery(".team-active").length > 0) {
 	let teamswiper = new Swiper('.team-active', {
 		slidesPerView: 4,
 		spaceBetween: 20,
 		loop: false,
-	  
+
 		// If we need pagination
 		pagination: {
 		  el: '.swiper-pagination',
 		  clickable: true,
 		},
-	  
+
 		// Navigation arrows
 		navigation: {
 		  nextEl: '.swiper-button-next',
 		  prevEl: '.swiper-button-prev',
 		},
-	  
+
 		// And if we need scrollbar
 		scrollbar: {
 		  el: '.swiper-scrollbar',
@@ -253,8 +253,8 @@
 			a11y: false
 		})
 	}
-	  
-  
+
+
 
 	$('.testimonial__slider ').owlCarousel({
 		loop:true,
@@ -301,19 +301,19 @@
 			autoplay: {
 					delay: 5000,
 				},
-		  
+
 			// If we need pagination
 			pagination: {
 			  el: '.swiper-pagination',
 			  clickable: true,
 			},
-		  
+
 			// Navigation arrows
 			navigation: {
 			  nextEl: '.swiper-button-next',
 			  prevEl: '.swiper-button-prev',
 			},
-		  
+
 			// And if we need scrollbar
 			scrollbar: {
 			  el: '.swiper-scrollbar',
@@ -329,11 +329,11 @@
 				  slidesPerView: 5,
 				},
 			  }
-	
+
 		  });
 		}
 
-	
+
 	////////////////////////////////////////////////////
     // 19. Masonary Js
 	$('.row-portfolio').imagesLoaded( function() {
@@ -417,13 +417,13 @@
 		delay: 10,
 		time: 1000
 	});
-	
+
 
 	if ($('.scene').length > 0 ) {
 		$('.scene').parallax({
 			scalarX: 10.0,
 			scalarY: 15.0,
-		}); 
+		});
 	};
 
 	////////////////////////////////////////////////////
@@ -450,3 +450,13 @@
 	}
 
 })(jQuery);
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('form').forEach(form => {
+        form.setAttribute('autocomplete', 'off');
+    });
+
+    document.querySelectorAll('input, textarea, select').forEach(field => {
+        field.setAttribute('autocomplete', 'off');
+    });
+});
